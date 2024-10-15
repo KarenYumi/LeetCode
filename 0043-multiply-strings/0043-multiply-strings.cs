277e496cba2b6975a605a9ac91ade04ecbe8b9cb
Yumi <148996314+KarenYumi@users.noreply.github.com>
2024-10-15 00:18:49 -0300

    Time: 70 ms (35.35%), Space: 40.8 MB (96.98%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.cs
+++ 0043-multiply-strings/0043-multiply-strings.cs
@@ -1,6 +1,6 @@
 public class Solution {
     public string Multiply(string num1, string num2) {
-        BigInteger i = BigInteger.Parse(num1);
+        BigInteger i = BigInteger.Parse(num1); //usado BigInteger por conta de sua capacidade de 64 bits enquanto int é de 32bits;
         BigInteger j = BigInteger.Parse(num2);
         BigInteger resultado = i*j;
         return resultado.ToString();
